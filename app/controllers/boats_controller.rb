33e3de012a584986df743badfb27e787a73ea250
aumugisha <arthur.umugisha@gmail.com>
2021-07-31 18:12:38 +0200

    boat controller beginning

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,31 @@
 class BoatsController < ApplicationController
+    before_action :set_boat
+
+    def new
+        @boat = Boat.new
+    end
+
+    def create
+      @boat = Boat.new(boat_params)
+      @boat.user_id = params[:user_id]
+      if boat.save
+        redirect_to boats_path
+      else
+        render "new"
+    end
+
+    def index
+        @boats = Boats.all
+    end
+
+    private
+
+    def set_boat
+      @boat = Boat.find(params[:boat_id])
+    end
+
+    def boat_params
+      params.require(:boat).permit(:name, :type, :description, :price_per_day, :location, :user_id)
+    end
+
 end
