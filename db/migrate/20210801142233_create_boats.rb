a9dbcda2a0099fa812f0cff671babeb423cdf9f0
aumugisha <arthur.umugisha@gmail.com>
2021-08-07 01:35:59 +0200

    geocoder added

--- db/migrate/20210801142233_create_boats.rb
+++ db/migrate/20210801142233_create_boats.rb
@@ -5,7 +5,7 @@ class CreateBoats < ActiveRecord::Migration[6.0]
       t.string :category
       t.text :description
       t.float :price_per_day
-      t.string :location
+      t.string :address
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_183730) do
+ActiveRecord::Schema.define(version: 2021_08_06_221814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,10 +41,12 @@ ActiveRecord::Schema.define(version: 2021_08_03_183730) do
     t.string "category"
     t.text "description"
     t.float "price_per_day"
-    t.string "location"
+    t.string "address"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
