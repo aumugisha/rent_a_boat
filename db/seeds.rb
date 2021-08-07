e791f9d0edafe944e356bbab4eb4af170043018d
aumugisha <arthur.umugisha@gmail.com>
2021-08-07 14:56:41 +0200

    new updates bookings controller + map

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +7,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.destroy
+# Boat.destroy
+
 user = User.create(
   email: "test@test.com", 
   password: "12345678"
@@ -16,12 +22,13 @@ user_two = User.create(
 )
 category = ["large boats", "medium boats", "small boats"]
 10.times do 
-  Boat.create(
+  boat = Boat.create(
     user_id: user.id, 
     name: Faker::Name.name,
     category: category.sample,
     description: Faker::Lorem.sentence,
     price_per_day: (1..1000).to_a.sample.to_f,
-    location:Faker::Address.full_address 
-  )
+    address:Faker::Address.full_address )
+
 end
+
